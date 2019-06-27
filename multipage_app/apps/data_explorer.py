cea9958a96cc105f45a807d04404036e10a803e7
Ray <rayees.rahman@icahn.mssm.edu>
2019-06-27 11:15:10 -0400

    Removed bootstrap components

--- multipage_app/apps/data_explorer.py
+++ multipage_app/apps/data_explorer.py
@@ -13,7 +13,6 @@ This page allows the user to explore the database
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import base64
 import string
